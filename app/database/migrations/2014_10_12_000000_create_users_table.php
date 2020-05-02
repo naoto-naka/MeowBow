6e025fc10e3d061ad47e2decea58f5880f45bfe8
naoto-naka <luckywhitecat770@gmail.com>
2020-05-02 14:42:31 +0000

    :shirt: Update profile_image column
    nullableの設定を忘れていた
    
    ＃====絵文字====
    ＃🐛：bug：バグ修正
    ＃👍：+1：機能改善
    ＃✨：sparkles：部分的な機能追加
    ＃🎉：tada：盛大に祝うべき大きな機能追加
    ＃♻️：recycle：リファクタリング
    ＃🚿：shower：不要な機能・使われなくなった機能の削除
    ＃💚：green_heart：テストやCIの修正・改善
    ＃👕：shirt：Lintエラーの修正やコードスタイルの修正
    ＃🚀：rocket：パフォーマンス改善
    ＃🆙：up：依存パッケージなどのアップデート
    ＃🔒：lock：新機能の公開範囲の制限
    ＃👮：cop：セキュリティ関連の改善

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('screen_name')->unique()->comment('アカウント名');
             $table->string('name')->comment('ユーザ名');
-            $table->string('profile_image')->comment('プロフィール写真');
+            $table->string('profile_image')->nullable()->comment('プロフィール写真');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
