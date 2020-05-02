957cf3d2147c75f4984cbd349d844f6c5f44f0ef
naoto-naka <luckywhitecat770@gmail.com>
2020-05-02 12:30:27 +0000

    :sparkles: Update Seed files
    各機能のテスト用初期データ作成のため
    ＃====絵文字====
    ＃🐛：bug：バグ修正
    ＃👍：+1：機能改善
    ＃✨：sparkles：部分的な機能追加
    ＃🎉：tada：盛大に祝うべき大きな機能追加
    ＃♻️：recycle：リファクタリング
    ＃🚿：shower：不要な機能・使われなくなった機能の削除
    ＃💚：green_heart：テストやCIの修正・改善
    ＃👕：shirt：Lintエラーの修正やコードスタイルの修正
    ＃🚀：rocket：パフォーマンス改善
    ＃🆙：up：依存パッケージなどのアップデート
    ＃🔒：lock：新機能の公開範囲の制限
    ＃👮：cop：セキュリティ関連の改善

--- app/database/seeds/FollowersTableSeeder.php
+++ app/database/seeds/FollowersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Follower;
+use Carbon\Carbon;
 
 class FollowersTableSeeder extends Seeder
 {
@@ -11,6 +13,11 @@ class FollowersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 2; $i <= 10; $i++) {
+          Follower::create([
+          'following_id' => $i,
+          'followed_id'  => 1,
+          ]);
+        }
     }
 }
--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -13,6 +13,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+      for ($i = 1; $i <= 10; $i++) {
+        User::create([
+          'screen_name'    => 'test_user'.$i,
+          'name'           => 'Test'.$i,
+          'profile_image'  => 'https://placehold.jp/50x50.png',
+          'email'          => 'test'.$i.'@test.com',
+          'password'       => Hash::make('1234567'),
+          'remember_token' => str_random(10),
+          'created_at'     => Carbon::now(),
+          'updated_at'     => Carbon::now(),
+        ]);
+      }
     }
 }
